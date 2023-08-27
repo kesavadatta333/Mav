edb877fb6fbb7457058ab356f4e079904a796dac
kesavadatta333 <142982386+kesavadatta333@users.noreply.github.com>
2023-08-27 23:51:35 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,10 @@
-package com.mycompany.app;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello world!" );
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world!"); // Use the logger to print log messages
     }
 }
